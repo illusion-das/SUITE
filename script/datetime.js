dba827154200c38127a30e15d81e9f5e7a1cca43
BackGwa <backgwa@icloud.com>
2023-08-03 22:04:45 +0900

    시간을 정상적으로 표시하지 못하던 버그 수정

--- script/datetime.js
+++ script/datetime.js
@@ -3,5 +3,5 @@ const appbar_datetime = document.getElementById("datetime");
 setInterval(() => {
     today = new Date();
 
-    appbar_datetime.innerHTML = today.toLocaleTimeString().substr(0, 7);
+    appbar_datetime.innerHTML = today.toLocaleTimeString().substr(0, 8);
 }, 500);
