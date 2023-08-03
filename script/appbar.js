f637d44bb159ae3acfeb2d20d886a115ef734f22
BackGwa <backgwa@icloud.com>
2023-08-03 22:04:55 +0900

    메뉴바 확장

--- script/appbar.js
+++ script/appbar.js
@@ -1,19 +1,36 @@
 const appbar = document.getElementById("appbar");
+let ff_appbar = false;
 
 // 터치 이벤트
-appbar.addEventListener("touchstart", () => {
-    appbar.classList.add("content-focus");
-})
-
-appbar.addEventListener("touchend", () => {
-    appbar.classList.remove("content-focus");
-})
+appbar.addEventListener("touchstart", () => { appbar_event(true) })
+appbar.addEventListener("touchend", () => { appbar_event(false) })
 
 // 마우스 이벤트
-appbar.addEventListener("mousedown", () => {
-    appbar.classList.add("content-focus");
-})
+appbar.addEventListener("mousedown", () => { appbar_event(true) })
+appbar.addEventListener("mouseup", () => { appbar_event(false) })
 
-appbar.addEventListener("mouseup", () => {
-    appbar.classList.remove("content-focus");
-})
+function appbar_event(focus) {
+    if (focus) {
+        focus_time = 0;
+        focus_interval = setInterval(() => {
+            focus_time += 1;
+            if (focus_time > 5 && !ff_appbar) {
+                control_center(true);
+                appbar_event(false);
+            } else if (focus_time > 5 && ff_appbar) {
+                control_center(false);
+                appbar_event(false);
+            }
+        }, 50);
+        appbar.classList.add("content-focus");
+    } else {
+        appbar.classList.remove("content-focus");
+        clearInterval(focus_interval);
+    }
+}
+
+function control_center(shown) {
+    if (shown) appbar.classList.add("control-center");
+    else appbar.classList.remove("control-center");
+    ff_appbar = shown;
+}
