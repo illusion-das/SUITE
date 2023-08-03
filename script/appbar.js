a5a44e299ec24390c0e4b784339c9792debce353
BackGwa <backgwa@icloud.com>
2023-08-03 21:13:03 +0900

    SUITE UI

--- script/appbar.js
+++ script/appbar.js
@@ -1,9 +1,19 @@
 const appbar = document.getElementById("appbar");
 
+// 터치 이벤트
 appbar.addEventListener("touchstart", () => {
     appbar.classList.add("content-focus");
 })
 
 appbar.addEventListener("touchend", () => {
+    appbar.classList.remove("content-focus");
+})
+
+// 마우스 이벤트
+appbar.addEventListener("mousedown", () => {
+    appbar.classList.add("content-focus");
+})
+
+appbar.addEventListener("mouseup", () => {
     appbar.classList.remove("content-focus");
 })
