39280321baccfc563664d44dda87064173abb824
BackGwa <backgwa@icloud.com>
2023-08-12 21:44:52 +0900

    test

--- script/datetime.js
+++ script/datetime.js
@@ -1,7 +1,15 @@
-const appbar_datetime = document.getElementById("datetime");
+try {
+    const appbar_datetime = document.getElementById("datetime");
+    setInterval(() => {
+        today = new Date();
+        appbar_datetime.innerHTML = today.toLocaleTimeString().slice(0, -3);
+    }, 250);   
+} catch {}
 
-setInterval(() => {
-    today = new Date();
-
-    appbar_datetime.innerHTML = today.toLocaleTimeString().slice(0, -3);
-}, 250);
+try {
+    const aod = document.getElementById("datetime-nonpm");
+    setInterval(() => {
+        today = new Date();
+        aod.innerHTML = `${(today.getHours() < 10) ? "0" + today.getHours():today.getHours()}:${(today.getMinutes() < 10) ? "0" + today.getMinutes():today.getMinutes()}`;
+    }, 250);
+} catch {}
