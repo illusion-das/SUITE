9122219a9dac139292e8b55011351c94ff543d70
BackGwa <backgwa@icloud.com>
2023-08-24 19:34:57 +0900

    UI update

--- script/aod.js
+++ script/aod.js
@@ -1,7 +1,8 @@
 const aod = document.querySelector("aod-screen");
+const datetime = document.querySelector("aod-datetime");
 
 setTimeout(() => {
-    document.querySelector("aod-datetime").classList.add("aodfade");
+    datetime.classList.add("aodfade");
 }, 400);
 
 aod.addEventListener("touchstart", () => { focus_on(true) })
@@ -17,13 +18,14 @@ function focus_on(is) {
 
         focus_interval = setInterval(() => {
             focus_time += 1;
-            if (focus_time > 40) {
-                location.href = "app.html";
+            if (focus_time > 80) {
+                location.href = "home.html";
             }
-        }, 25);
+        }, 10);
     }
     else {
         clearInterval(focus_interval);
         aod.classList.remove("click-focus");
     }
+
 }
